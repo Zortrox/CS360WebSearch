b47d77c0fdff892ffaf776e0e78f7fa12575e890
Zortrox <zortroxofdoom@gmail.com>
2015-09-23 11:45:44 -0500

    first DB php test

--- site/search/index.php
+++ site/search/index.php
@@ -16,6 +16,27 @@
 	</form>
 	<div id="results-list">
 		<p>WOW, LOOK AT ALL THE RESULTS!</p>
+		<?php 
+			$mysqli = new mysqli("localhost", "cssearch", "WKU!searchsql", "webSearchEngine");
+
+			/* check connection */
+			if (mysqli_connect_errno()) {
+				printf("Connect failed: %s\n", mysqli_connect_error());
+				exit();
+			}
+
+			/* return name of current default database */
+			if ($result = $mysqli->query("SELECT * FROM keywords")) {
+				if ($result->num_rows > 0) {
+					$row = $result->fetch_row();
+					printf("Default database is %s.\n", $row[0]);
+					$result->close();
+				} else {
+					printf("No Data");
+				}
+				
+			}
+		?>
 	</div>
 </div>
 </body>
