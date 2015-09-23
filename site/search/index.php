749aa1921cef2e8c6448ea9fa42eba1def5e9624
Zortrox <zortroxofdoom@gmail.com>
2015-09-23 14:36:36 -0500

    added everything back to main file

--- site/search/index.php
+++ site/search/index.php
@@ -19,7 +19,39 @@
 	</div>
 	<div id="results-list">
 		<p>WOW, LOOK AT ALL THE RESULTS!</p>
-		<?php include "results.php"; ?>
+		<?php
+		//include "results.php";
+		//$query variable is the user's search
+
+	$user = "web";
+	$pass = "webSearch!"
+	$database = "webSearchEngine"
+
+	printf("start");
+
+	$mysqli = new mysqli("localhost", $user, $pass, $database);
+
+	/* check connection */
+	if (mysqli_connect_errno()) {
+		printf("Connect failed: %s\n", mysqli_connect_error());
+		exit();
+	}
+
+	$sql = "SELECT * FROM keywords";
+
+	/* return name of current default database */
+	if ($result = $mysqli->query($sql)) {
+		if ($result->num_rows > 0) {
+			$row = $result->fetch_row();
+			printf("Default database is %s.\n", $row[0]);
+			$result->close();
+		} else {
+			printf("No Data");
+		}
+	}
+
+	printf("end");
+		?>
 	</div>
 </div>
 </body>
