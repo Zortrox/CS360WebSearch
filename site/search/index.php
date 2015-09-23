9888105e5806eb67c7017eb3a024f5d1edec2760
Zortrox <zortroxofdoom@gmail.com>
2015-09-23 14:05:52 -0500

    moved search php into new file.  maybe queried for table names

--- site/search/index.php
+++ site/search/index.php
@@ -19,27 +19,7 @@
 	</div>
 	<div id="results-list">
 		<p>WOW, LOOK AT ALL THE RESULTS!</p>
-		<?php 
-			$mysqli = new mysqli("localhost", "cssearch", "WKU!searchsql", "webSearchEngine");
-
-			/* check connection */
-			if (mysqli_connect_errno()) {
-				printf("Connect failed: %s\n", mysqli_connect_error());
-				exit();
-			}
-
-			/* return name of current default database */
-			if ($result = $mysqli->query("SELECT * FROM keywords")) {
-				if ($result->num_rows > 0) {
-					$row = $result->fetch_row();
-					printf("Default database is %s.\n", $row[0]);
-					$result->close();
-				} else {
-					printf("No Data");
-				}
-				
-			}
-		?>
+		<?php include "results.php"; ?>
 	</div>
 </div>
 </body>
---
+++ site/search/results.php
@@ -0,0 +1,31 @@
+<?php
+//$query variable is the user's search
+
+$user = "web";
+$pass = "webSearch!"
+$database = "webSearchEngine"
+
+$mysqli = new mysqli("localhost", $user, $pass, $database);
+
+/* check connection */
+if (mysqli_connect_errno()) {
+	printf("Connect failed: %s\n", mysqli_connect_error());
+	exit();
+}
+
+$sql = "SELECT TABLE_NAME 
+FROM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_TYPE = 'BASE TABLE' AND TABLE_SCHEMA=" . $database;
+
+/* return name of current default database */
+if ($result = $mysqli->query("SELECT * FROM keywords")) {
+	if ($result->num_rows > 0) {
+		$row = $result->fetch_row();
+		printf("Default database is %s.\n", $row[0]);
+		$result->close();
+	} else {
+		printf("No Data");
+	}
+}
+
+?>
