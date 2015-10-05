d0d5e23c48401579b2d884735919cc3f167ada32
reviveyi819 <snlove@vip.qq.com>
2015-10-05 08:48:24 -0500

    Update results.php
    I was update the search function down to the old function

--- site/search/results.php
+++ site/search/results.php
@@ -27,3 +27,57 @@ if ($result = $mysqli->query($sql)) {
 }
 
 ?>
+---------------------------------------------------------------------------
+<?php
+
+	$button = $_GET [ 'submit' ];
+	$search = $_GET [ 'search' ]; 
+	
+	$user = "web";
+	$pass = "webSearch!";
+	$database = "webSearchEngine";
+	
+	
+	if( !$button )
+		echo "you didn't submit a keyword";
+    else {
+			if( strlen( $search ) <= 1 )
+				echo "you keyword is too short";
+			else {
+			     echo "You searched for <b> $search </b> <hr size='1' > </ br > ";
+				 mysql_connect( "localhost","web","webSearch!") ; 
+				 mysql_select_db("webSearchEngine");
+				 
+				 $search_exploded = explode ( " ", $search );
+				 $x = 0; 
+				 foreach( $search_exploded as $search_each ) {
+						$x++;
+					    $construct = "";
+					    if( $x == 1 )
+								$construct .="keywords LIKE '%$search_each%'";
+						else		
+								$construct .="AND keywords LIKE '%$search_each%'";
+                    }
+					
+					$construct = " SELECT * FROM SEARCH_ENGINE WHERE $construct ";
+					$run = mysql_query( $construct );
+					
+					$foundnum = mysql_num_rows($run);
+
+					if ($foundnum == 0)
+							echo "Sorry, there are no matching result for <b> $search </b>. </br> </br> 
+							     
+						else {		 
+								echo "$foundnum results found !<p>";
+							
+							while( $runrows = mysql_fetch_assoc( $run ) ) { 
+								$title = $runrows ['title']; 
+								$desc = $runrows ['description']; 
+								$url = $runrows ['url'];
+								
+								echo "<a href='$url'> <b> $title </b> </a> <br> $desc <br> <a href='$url'> $url </a> <p>";
+								}
+					}			
+			}
+	}
+?>
