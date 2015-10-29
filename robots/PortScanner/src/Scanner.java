aad470802d30df18b9767ee32f46d156ef058e11
Zortrox <zortroxofdoom@gmail.com>
2015-10-29 00:54:15 -0500

    Maybe added database insertion to port scanner

--- robots/PortScanner/src/Scanner.java
+++ robots/PortScanner/src/Scanner.java
@@ -1,23 +1,34 @@
-import java.io.*;
 import java.util.concurrent.*;
 
 public class Scanner {
 
-    public static void main(String[] args) throws IOException, InterruptedException {
-    	PrintWriter writer = new PrintWriter("serverIPList.txt", "UTF-8");
+    public static void main(String[] args) throws InterruptedException {
+    	long startTime = System.nanoTime();
+    	
+    	DatabaseManager.Initialize();
     	
     	Semaphore s = new Semaphore(255);
     	
     	for (int i=0; i<256; i++) {
     		for (int j=0; j<256; j++) {
-    			s.acquire();
-    			Runnable r = new PortThread(i, j, s);
-    			new Thread(r).start();
+    			for (int p=0; p<2; p++) {
+	    			s.acquire();
+	    			Runnable r = new PortThread(i, j, p, s);
+	    			new Thread(r).start();
+    			}
     		}
     	}
     	
-    	System.out.println("DONE");
+    	long totalTime = (System.nanoTime() - startTime);
     	
-    	writer.close();
+    	//get number of actual web servers from database
+    	
+    	DatabaseManager.Exit();
+    	
+    	System.out.println("DONE -- ");
+    	System.out.println("65535 IP addresses parsed in "
+    			+ "" + TimeUnit.NANOSECONDS.toHours(totalTime) + " hours "
+				+ "" + TimeUnit.NANOSECONDS.toMinutes(totalTime) + " minutes "
+				+ "" + TimeUnit.NANOSECONDS.toSeconds(totalTime) + " seconds.");
     }
 }
