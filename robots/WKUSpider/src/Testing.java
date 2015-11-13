a24a085f806b1c63309efe66f43eb0a1ae80ae9d
Furil <ploaj_the_otter@yahoo.com>
2015-11-13 13:10:40 -0600

    Added the port scanner stuff to the driver... Probably shouldn't run it yet, though

--- robots/WKUSpider/src/Testing.java
+++ robots/WKUSpider/src/Testing.java
@@ -17,7 +17,7 @@ public class Testing {
 //		if(1==1)
 //			return;
 		
-		boolean start = false;
+		boolean start = false, portscan = false;
 		String displayData = "";
 		int numOfThreads = 10;
 		int amt = 0;
@@ -26,6 +26,7 @@ public class Testing {
 		if(args.length == 0){
 			System.out.println("No parameters were entered.\n"
 					+ "-start \t\t: starts the spider\n"
+					+ "-startPortScan \t: starts the port scan indexing\n"
 					+ "-display \t: enter the database name to display its contents\n"
 					+ "-amt \t\t: set the amount of pages for each thread to transverse\n"
 					+ "-numOfThreads \t: set the maximum number of threads to use\n"
@@ -37,6 +38,9 @@ public class Testing {
 			switch(args[i]){
 			case "-start":
 				start = true;
+				break;
+			case "-startPortScan":
+				portscan = true;
 				break;
 			case "-display":
 				displayData = args[i+1];
@@ -72,6 +76,9 @@ public class Testing {
 		case "siteKeywords":
 			DatabaseManager.printDataDatabase();
 			break;
+		case "webServers":
+			DatabaseManager.printWebServers();
+			break;
 		}
 		
 		
@@ -80,6 +87,22 @@ public class Testing {
 			
 			System.out.println("Begin Crawling");
 			SpiderThread th = new SpiderThread(numOfThreads, startingPoint, amt);
+
+			long totalTime = (System.nanoTime() - startTime);
+			System.out.println(th.numOfPages + " pages crawled in " + ""
+					+ TimeUnit.NANOSECONDS.toHours(totalTime) + " hours " + ""
+					+ TimeUnit.NANOSECONDS.toMinutes(totalTime)%60 + " minutes "
+					+ "" + TimeUnit.NANOSECONDS.toSeconds(totalTime)%60
+					+ " seconds " + ""
+					+ TimeUnit.NANOSECONDS.toMillis(totalTime)%1000
+					+ " milliseconds");
+		}
+		
+		if (portscan) {
+			long startTime = System.nanoTime();
+			
+			System.out.println("Begin Crawling Port Scanner Version!");
+			SpiderThread th = new SpiderThread(numOfThreads, amt);
 
 			long totalTime = (System.nanoTime() - startTime);
 			System.out.println(th.numOfPages + " pages crawled in " + ""
