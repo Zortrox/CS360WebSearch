028d5b0ead24e116aab388951e3159f316a15145
Zortrox <zortroxofdoom@gmail.com>
2015-11-18 14:34:54 -0600

    test echos

--- site/search/results.php
+++ site/search/results.php
@@ -73,9 +73,11 @@ foreach ($stringArray as $search) {
 	$words = explode(" ",trim($search));
 	$keyIdQuery = "SELECT keyId FROM keywords WHERE word LIKE " . $words[0];
 	$keyIdRows = $mysqli->query($keyIdQuery);
+	echo $keyIdRows->num_rows;
 	$keyResult = $keyIdRows->fetch_row();
 	$webIdQuery = "SELECT * FROM siteKeywords WHERE keyId LIKE " . $keyResult[0];
 	$webIdRows = $mysqli->query($webIdQuery);
+	echo $webIdRows->num_rows;
 
 	$thisWordArray = array();
 	while ($webId = $webIdRows->fetch_row()) {
