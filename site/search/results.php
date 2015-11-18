49609bc0f4f390ddc9bc936b09df40ada66bad2d
Zortrox <zortroxofdoom@gmail.com>
2015-11-18 15:21:19 -0600

    change to include quotes

--- site/search/results.php
+++ site/search/results.php
@@ -71,7 +71,7 @@ $stringIds = array();
 $stringIndex = 0;
 foreach ($stringSearch[1] as $search) {
 	$words = preg_split('/\s+/', trim($search));
-	$keyIdQuery = "SELECT keyId FROM keywords WHERE word LIKE " . $words[0];
+	$keyIdQuery = "SELECT keyId FROM keywords WHERE word LIKE \"" . $words[0] . "\"";
 	$keyIdRows = $mysqli->query($keyIdQuery);
 
 	echo $keyIdRows->num_rows;
