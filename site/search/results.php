8657ed7268c390af714f17b6c0d153e3f2e6444f
Zortrox <zortroxofdoom@gmail.com>
2015-11-18 14:40:43 -0600

    separate if there are rows found

--- site/search/results.php
+++ site/search/results.php
@@ -73,19 +73,19 @@ foreach ($stringSearch as $search) {
 	$words = explode(" ",trim($search));
 	$keyIdQuery = "SELECT keyId FROM keywords WHERE word LIKE " . $words[0];
 	$keyIdRows = $mysqli->query($keyIdQuery);
-	echo "key";
-	echo $keyIdRows->num_rows;
-	$keyResult = $keyIdRows->fetch_row();
-	$webIdQuery = "SELECT * FROM siteKeywords WHERE keyId LIKE " . $keyResult[0];
-	$webIdRows = $mysqli->query($webIdQuery);
-	echo "web";
-	echo $webIdRows->num_rows;
 
-	$thisWordArray = array();
-	while ($webId = $webIdRows->fetch_row()) {
-		array_push($thisWordArray, $webId[0]);
+	if ($keyIdRows->num_rows > 0) {
+		$keyResult = $keyIdRows->fetch_row();
+		$webIdQuery = "SELECT * FROM siteKeywords WHERE keyId LIKE " . $keyResult[0];
+		$webIdRows = $mysqli->query($webIdQuery);
+
+		$thisWordArray = array();
+		while ($webId = $webIdRows->fetch_row()) {
+			array_push($thisWordArray, $webId[0]);
+		}
+		$stringIds[$stringIndex] = $thisWordArray;
 	}
-	$stringIds[$stringIndex] = $thisWordArray;
+	
 	$stringIndex++;
 }
 print_r($stringIds);
