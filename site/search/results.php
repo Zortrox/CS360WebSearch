88b3d3b1f3c7926bcbc75fb9216c615c2428d815
Zortrox <zortroxofdoom@gmail.com>
2015-11-30 14:09:12 -0600

    allow weights when debug mode on

--- site/search/results.php
+++ site/search/results.php
@@ -257,6 +257,8 @@ else {
 		}
 		for ($i=$offset; $i<$sitesFound; $i++) {
 			$linkWeight = $webArray[$linkArray[$i]['webId']];
+			$debugWeight = "";
+			if ($doDebug) $debugWeight = " - " . $linkWeight;
 			$title = $linkArray[$i]['name'];
 			if ($title == "") $title = $linkArray[$i]['url'];
 			$desc = $linkArray[$i]['description'];
@@ -266,7 +268,7 @@ else {
 				$url = "http://" . $url;
 			}
 
-			echo "<p><a href='$url' class='title' style='font-family:ariel,sans-serif;'> <b> $title - $linkWeight </b> </a> <br> $desc <br> <a href='$url' style='text-decoration:none;'> $url </a></p>";
+			echo "<p><a href='$url' class='title' style='font-family:ariel,sans-serif;'> <b> $title$debugWeight </b> </a> <br> $desc <br> <a href='$url' style='text-decoration:none;'> $url </a></p>";
 
 			//only go for 10 pages
 			if ($i-$offset == 9) break;
