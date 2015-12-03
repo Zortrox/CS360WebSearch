bd77b8ae100e8e20f47f31d7a3ac80d5991b1981
Taylor <taylor@faceyspacies.com>
2015-12-03 13:40:04 -0600

    updated sql files and document
    title

--- database/locations.sql
+++ database/locations.sql
@@ -3,5 +3,5 @@ CREATE TABLE IF NOT EXISTS locations(
 	name varchar(1000) NOT NULL,
 	description varchar(10000) NOT NULL,
 	url varchar(1500) NOT NULL,
-	siteFullText varchar(15000) NOT NULL
+	siteFullText longtext NOT NULL
 	);
